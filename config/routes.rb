7bd5e052fc794781a1202c5567b5c70cfd942927
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-08-18 18:40:06 +0200

    Create controlers and routes for libraries and users.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :movies, only: [:index]
   resources :seasons, only: [:index]
+  resources :users, only: [:index]
+  resources :libraries, only: [:index, :show]
   # Defines the root path route ("/")
   # root "articles#index"
 end
